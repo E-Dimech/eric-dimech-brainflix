dfa7623a71ce86b3d5ed106bf892f3a4b33df659
Eric Dimech <ericdimech@hotmail.com>
2020-09-15 23:27:54 -0400

    Added header and hero sections

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './components/Header/Header.scss'
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      
+      <Hero />
     </div>
   );
 }
--- sprint-1/src/components/Hero/Hero.jsx
+++ sprint-1/src/components/Hero/Hero.jsx
@@ -1,3 +1,20 @@
 import React from 'react';
+import Race from '../../assets/Images/video-list-0.jpg';
+import Play from '../../assets/Icons/SVG/Icon-play.svg';
+import FullScreen from '../../assets/Icons/SVG/Icon-fullscreen.svg'
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div>
+                <video className="hero__poster" poster={Race} />
+            </div>
+            <div>
+                <img className="hero__play" src={Play} alt="play button" />
+                <img className="hero__full-screen" src={FullScreen} alt="Full screen button" />
+            </div>
+        </div>
+    )
+};
+
 
 export default Hero;
